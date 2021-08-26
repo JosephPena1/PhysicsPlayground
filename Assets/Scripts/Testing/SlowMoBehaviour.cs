e288e71fa545074df3c5d222827ce7e50535ef82
Joseph Peña <jgpena2888@gmail.com>
2021-08-26 15:36:45 -0500

    Created Physic Materials, Added new prefabs, & added player;

---
+++ Assets/Scripts/Testing/SlowMoBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowMoBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private float _slowAmount;
+    private bool _timeSlowed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) && _timeSlowed)
+        {
+            Time.timeScale = 1;
+            _timeSlowed = false;
+        }
+            
+        if (Input.GetKeyDown(KeyCode.DownArrow) && Time.timeScale != 0)
+        {
+            Time.timeScale -= _slowAmount;
+            _timeSlowed = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Testing/TimeScaleTextBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeScaleTextBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private Text _displayText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _displayText.text = "" + 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _displayText.text = "TimeScale: " + Time.timeScale;
+    }
+}
