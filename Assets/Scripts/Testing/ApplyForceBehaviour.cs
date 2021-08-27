7e4c885d8725c20f94d12bd61167bd9f04004ed7
Joseph Peña <jgpena2888@gmail.com>
2021-08-27 14:32:51 -0500

    Fixed hinges & added motors to vehicle;

---
+++ Assets/Scripts/Testing/ApplyForceBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ApplyForceBehaviour : MonoBehaviour
+{
+    private Rigidbody _rigidBody;
+    [SerializeField]
+    private float _force;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+            _rigidBody.AddForce(new Vector3(0,0, _force), ForceMode.Impulse);
+    }
+}
