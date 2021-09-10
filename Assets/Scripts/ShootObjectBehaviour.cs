f1cf1a3e87014bae823a33ce84bac5cec2e41f69
Joseph Peña <jgpena2888@gmail.com>
2021-09-10 13:19:42 -0500

    Remade throwing hammer prefab;
    changed hammer to one object with 2 colliders;

--- Assets/Scripts/ShootObjectBehaviour.cs
+++ Assets/Scripts/ShootObjectBehaviour.cs
@@ -7,7 +7,6 @@ public class ShootObjectBehaviour : MonoBehaviour
     public Camera camera;
     public Transform spawnLocation;
     public Rigidbody rigidBody;
-    public float projectileTime = 2.0f;
 
     private void Update()
     {
@@ -24,19 +23,19 @@ public class ShootObjectBehaviour : MonoBehaviour
         Vector3 displacement = hit.point - camera.transform.position;
         Vector3 acceleration = Physics.gravity;
 
-        Vector3 initialVelocity = FindInitialVelocity(displacement, acceleration, projectileTime);
+        Vector3 initialVelocity = FindInitialVelocity(displacement, acceleration);
 
         Rigidbody projectileObject = Instantiate(rigidBody, spawnLocation.position, camera.transform.rotation);
         projectileObject.AddForce(initialVelocity, ForceMode.VelocityChange);
     }
 
-    private Vector3 FindInitialVelocity(Vector3 displacement, Vector3 acceleration, float time)
+    private Vector3 FindInitialVelocity(Vector3 displacement, Vector3 acceleration)
     {
         //Δx = v0*t + ½*a*t²
         //Δx - ½*a*t² = v0*t
         //Δx/t - ½*a*t = v0
         //v0 = Δx/t - ½*a*t
-        Vector3 initialVelocity = displacement - 0.5f * acceleration * time;
+        Vector3 initialVelocity = displacement - 0.5f * acceleration;
 
         return initialVelocity;
     }
